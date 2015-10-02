0952cdf1186d63d6a93591429d55515ef8801998
Vaishakh Nadgir <vaishakh.nadgir@ef.com>
2015-10-02 14:06:27 +0100

    Add default DateTimeFormat to parse/format DateTimes in UTC (ISO8601)

--- src/main/java/com/vnadgir/hal/HalApplication.java
+++ src/main/java/com/vnadgir/hal/HalApplication.java
@@ -1,5 +1,9 @@
 package com.vnadgir.hal;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
+import com.fasterxml.jackson.datatype.joda.ser.DateTimeSerializer;
 import com.theoryinpractise.halbuilder.api.RepresentationFactory;
 import com.theoryinpractise.halbuilder.standard.StandardRepresentationFactory;
 import com.vnadgir.hal.api.resources.TestSessionResource;
@@ -9,11 +13,16 @@ import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.setup.Environment;
 
 import javax.ws.rs.client.Client;
+import java.text.SimpleDateFormat;
 
 public class HalApplication extends io.dropwizard.Application<HalConfiguration> {
 
     @Override
     public void run(HalConfiguration halConfiguration, Environment environment) throws Exception {
+        ObjectMapper objectMapper = environment.getObjectMapper();
+        objectMapper.registerModule(new JodaModule());
+        objectMapper.setDateFormat(new ISO8601DateFormat());
+
         final Client client = new JerseyClientBuilder(environment)
                 .build(getName());
 
